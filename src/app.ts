cec74479d47294111ca0e717e15b10b468648109
Gabriela Ramos <gabyr.contact@gmail.com>
2024-10-07 16:55:52 -0500

    añadiendo ruta raiz

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,22 @@ import userRoutes from './routes/user';
 import { PrismaClient } from '@prisma/client'
 import { loginFunction } from './routes/authentication';
 import { jwtMiddleware } from './middleware/authorization';
+import dotenv from 'dotenv'; // Importa dotenv
+
+dotenv.config(); // Carga las variables de entorno
 
 const app: Application = express();
-
 const PORT: number = 3001;
-
-
 const prisma = new PrismaClient()
 
 app.use(express.json());
 app.use(jwtMiddleware('secret_key'));
+
+// Ruta para la raíz '/'
+app.get('/', (req: Request, res: Response): void => {
+    res.send('Hello! Welcome to the Fleet Management API.');
+  });
+
 app.use('/api/taxis', taxisRoutes);
 app.use('/api/trajectories', trajectoriesRoutes);
 app.use('/api/users', userRoutes);
